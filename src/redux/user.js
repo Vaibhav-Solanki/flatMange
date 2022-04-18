60246d52e21442ea774dfc3a77624bd057c4364b
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-04-18 09:10:28 +0530

    Update user.js

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,10 +1,20 @@
-const initialState = { state: false, token: "" };
+const initialState = JSON.parse(localStorage.getItem("user")) || {
+  state: false,
+  token: "",
+};
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case "login":
+    case "login": {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ state: true, token: payload })
+      );
       return { state: true, token: payload };
-    case "logout":
+    }
+    case "logout": {
+      localStorage.setItem("user", JSON.stringify({ state: false, token: "" }));
       return { state: false, token: "" };
+    }
     default:
       return state;
   }
